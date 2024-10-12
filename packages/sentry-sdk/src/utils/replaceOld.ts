ec8bf69f9281370a3d4c48c84cc55b8349227081
LiuJie <1765659336@qq.com>
2024-10-12 11:07:00 +0800

    feat: v

--- packages/sentry-sdk/src/utils/replaceOld.ts
+++
@@ -1,8 +0,0 @@
-export default (source: { [key: string]: any }, name: string, replacement: (...arg: any[]) => any): void => {
-	if (!(name in source)) return;
-	const original = source[name];
-	const warpped = replacement(original);
-	if (typeof warpped === 'function') {
-		source[name] = warpped;
-	}
-};
